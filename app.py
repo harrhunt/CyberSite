0cba14406f4ace493c0c1c68c7d99c90910d5536
harrhunt <harrhunt@isu.edu>
2021-07-27 18:06:03 -0600

    abstracted database models into separate database file

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from datetime import date, datetime
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from database import *
 import sass
 from flaskconf import SELECTED_CONFIG
 
@@ -12,44 +13,6 @@ app = Flask(__name__)
 app.config.from_object(SELECTED_CONFIG)
 
 db = SQLAlchemy(app)
-
-
-class Area(db.Model):
-    __tablename__ = 'areas'
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(100))
-
-
-class Unit(db.Model):
-    __tablename__ = 'units'
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(100))
-
-
-class Keyword(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(100))
-
-
-class Module(db.Model):
-    __tablename__ = 'keywords'
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(100))
-    author = db.Column(db.String(100))
-    date_added = db.Column(db.Date, default=date.today())
-    description = db.Column(db.String(8192))
-    notes = db.Column(db.String(8192))
-
-
-class File(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(200), unique=True)
-    date_added = db.Column(db.Date, default=date.today())
-
-
-class Source(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    url = db.Column(db.String(300), unique=True)
 
 
 @app.route('/')
---
+++ database.py
@@ -0,0 +1,41 @@
+from flask_sqlalchemy import Model
+from sqlalchemy.types import Integer, String, Date
+from sqlalchemy import Column
+
+
+class Area(Model):
+    __tablename__ = 'areas'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(100))
+
+
+class Unit(Model):
+    __tablename__ = 'units'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(100))
+
+
+class Keyword(Model):
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(100))
+
+
+class Module(Model):
+    __tablename__ = 'keywords'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(100))
+    author = Column(String(100))
+    date_added = Column(Date, default=date.today())
+    description = Column(String(8192))
+    notes = Column(String(8192))
+
+
+class File(Model):
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(200), unique=True)
+    date_added = Column(Date, default=date.today())
+
+
+class Link(Model):
+    id = Column(Integer(), primary_key=True)
+    url = Column(String(300), unique=True)
