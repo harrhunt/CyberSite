d1d020645054ff1de3c2d318c7e4a36ffcc54bb1
harrhunt <harrhunt@isu.edu>
2021-08-03 14:17:17 -0600

    fixed issue with clicks on module page where module does not have data-id attr

--- static/scripts/tag_search.js
+++ static/scripts/tag_search.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
-    $(".modules-container").on('click', function (e) {
+    $(".module-container").on('click', function (e) {
         let target = $(e.target);
         if (target.is(".module")) {
-            location.href = `/module/${target.attr("data-id")}`;
+            if (target.attr("data-id")) {
+                location.href = `/module/${target.attr("data-id")}`;
+            }
         } else if (target.is(".unit") || target.is(".area")) {
             location.href = `/modules?${target.attr('class').split(/\s+/)[0]}=${target.text()}`;
         } else if (target.is(".keyword")) {
