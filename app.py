01138417a201b6ea9fe0fad7fac3ec3356da5a67
harrhunt <harrhunt@isu.edu>
2021-08-09 15:24:52 -0600

    added basic login logout control and user model

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, render_template_string, request, abort, send_from_directory, send_file
+from flask import Flask, render_template, render_template_string, request, abort, send_from_directory, send_file, redirect, url_for, flash
+from flask_login import LoginManager, login_required, UserMixin, login_user, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
 from werkzeug.utils import secure_filename
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date, datetime
 from zipfile import ZipFile, ZIP_DEFLATED
@@ -14,6 +19,17 @@ sass.compile(dirname=('static/styles/sass', 'static/styles/css'), output_style='
 app = Flask(__name__)
 app.config.from_object(SELECTED_CONFIG)
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+# login_manager.login_view = 'login'
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(24), unique=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(80))
 
 
 class Area(db.Model):
@@ -138,9 +154,46 @@ class Link(db.Model):
     url = db.Column(db.String(300), unique=True)
 
 
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email('Invalid email'), Length(max=100)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+
+@login_manager.user_loader
+def user_loader(user_id):
+    return User.query.get(user_id)
+
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'GET':
+        return render_template("login.html", form=form)
+    elif request.method == 'POST':
+        if form.validate_on_submit():
+            user = User.query.filter(User.username == form.username.data).first()
+            if user and check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                next_loc = request.args.get('next')
+                if next_loc:
+                    return redirect(url_for(next_loc))
+                else:
+                    return redirect(url_for('admin'))
+            flash("Credentials are incorrect", "error")
+            return render_template("login.html", form=form)
+        return render_template("login.html", form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 @app.route('/module/<module_id>')
